ed1df6e24f80253340ed2bdad8877c4b8ff64b0d
Rynoo1 <221361@virtualwindow.co.za>
2023-07-19 13:46:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Term3-webstore
+ Term 3 project -  E-commerce store
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello word</div>
+      <Routes>
+      {/* <Route path='/' element={<Landing/>} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
