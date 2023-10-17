7ada57b7bc6b84862b1367932f270c3849c5d5f7
Rynoo1 <221361@virtualwindow.co.za>
2023-10-17 14:55:56 +0200

    update

--- Server/routes/watches.js
+++ Server/routes/watches.js
@@ -5,38 +5,38 @@ const path = require('path');
 const multer = require('multer');
 
 //get all watches
-router.get('/api/getwatches', async(req, res) => {
+router.get('/api/getwatches', async (req, res) => {
     const findWatches = await WatchSchema.find()
     res.json(findWatches)
 })
 
 //get specific watch
-router.get('/api/watch/:id', async(req, res) => {
+router.get('/api/watch/:id', async (req, res) => {
     const findWatch = await WatchSchema.findById(req.params.id)
     res.json(findWatch)
 })
 
 //filter straps
-router.get('/api/strapwatches/:strap', async(req, res) => {
+router.get('/api/strapwatches/:strap', async (req, res) => {
     const findStraps = await WatchSchema.find()
-    .where("strap")
-    .in(req.params.strap)
+        .where("strap")
+        .in(req.params.strap)
     res.json(findStraps)
 })
 
 //filter brands
-router.get('/api/brandwatches/:brand', async(req, res) => {
+router.get('/api/brandwatches/:brand', async (req, res) => {
     const findBrands = await WatchSchema.find()
-    .where("brand")
-    .in(req.params.brand)
+        .where("brand")
+        .in(req.params.brand)
     res.json(findBrands)
 })
 
 //filter straps
-router.get('/api/strapwatches/:strap', async(req, res) => {
+router.get('/api/strapwatches/:strap', async (req, res) => {
     const findStraps = await WatchSchema.find()
-    .where("strap")
-    .in(req.params.brand)
+        .where("strap")
+        .in(req.params.brand)
     res.json(findStraps)
 })
 
@@ -55,48 +55,81 @@ const upload = multer({
 
 
 //add watches
-router.post('/api/addwatch', upload.single('image'), async(req, res) => {
-    
+router.post('/api/addwatch', upload.single('image'), async (req, res) => {
+
     let data = JSON.parse(req.body.information)
     // const watch = new WatchSchema({ ...req.body})
-    const watch = new WatchSchema({
-        brand: data.brand,
-        model: data.model,
-        year: data.year,
-        strap: data.strap,
-        size: data.size,
-        price: data.price,
-        stock: data.stock,
-        description: data.description,
-        image: req.file.filename
-    })
-    await watch.save()
-        .then(response => res.json(response))
-        .catch(error => res.status(500).json(error))
+    if (req.file) {
+        const watch = new WatchSchema({
+            brand: data.brand,
+            model: data.model,
+            year: data.year,
+            strap: data.strap,
+            size: data.size,
+            price: data.price,
+            stock: data.stock,
+            description: data.description,
+            image: req.file.filename
+        })
+        await watch.save()
+            .then(response => res.json(response))
+            .catch(error => res.status(500).json(error))
+    } else {
+        const watch = new WatchSchema({
+            brand: data.brand,
+            model: data.model,
+            year: data.year,
+            strap: data.strap,
+            size: data.size,
+            price: data.price,
+            stock: data.stock,
+            description: data.description,
+            image: ''
+        })
+        await watch.save()
+            .then(response => res.json(response))
+            .catch(error => res.status(500).json(error))
+    }
 })
 
 //update watches
-router.put('/api/watch/:id', upload.single('imageUp'), async(req, res) => {
+router.put('/api/watch/:id', upload.single('imageUp'), async (req, res) => {
 
     let data = JSON.parse(req.body.information)
-    const update = ({
-        brand: data.brand,
-        model: data.model,
-        year: data.year,
-        strap: data.strap,
-        size: data.size,
-        price: data.price,
-        stock: data.stock,
-        description: data.description,
-        image: req.file.filename
-    })
-    await WatchSchema.findByIdAndUpdate(req.params.id, update)
-        .then(response => res.json(response))
-        .catch(error => res.status(500).json(error))
+    if (req.file) {
+        const update = ({
+            brand: data.brand,
+            model: data.model,
+            year: data.year,
+            strap: data.strap,
+            size: data.size,
+            price: data.price,
+            stock: data.stock,
+            description: data.description,
+            image: req.file.filename
+        })
+        await WatchSchema.findByIdAndUpdate(req.params.id, update)
+            .then(response => res.json(response))
+            .catch(error => res.status(500).json(error))
+    } else {
+        const update = ({
+            brand: data.brand,
+            model: data.model,
+            year: data.year,
+            strap: data.strap,
+            size: data.size,
+            price: data.price,
+            stock: data.stock,
+            description: data.description
+        })
+        await WatchSchema.findByIdAndUpdate(req.params.id, update)
+            .then(response => res.json(response))
+            .catch(error => res.status(500).json(error))
+    }
 })
 
 //delete
-router.delete('/api/watch/:id', async(req, res) => {
+router.delete('/api/watch/:id', async (req, res) => {
     await WatchSchema.findByIdAndDelete(req.params.id)
         .then(response => res.json(response))
         .catch(error => res.status(500).json(error))
